07d5999877aab369b0faae25afd9132940272f36
Cassandra Culkins and Riley Starnes <rstarnes9@gmail.com>
2016-03-15 11:31:00 -0700

    add deployment server

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var uglify = require("gulp-uglify");
 var utilities = require("gulp-util");
 var jshint = require('gulp-jshint');
 var source = require("vinyl-source-stream");
+var browserSync = require('browser-sync').create();
 var lib = require('bower-files')({
   "overrides":{
     "bootstrap" : {
@@ -72,3 +73,22 @@ gulp.task('bowerCSS', function(){
 });
 
 gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
+gulp.task('serve', function(){
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
+});
